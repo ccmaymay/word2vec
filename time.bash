db0bdb4ccd31fe158276d20964fa2e55638e609d
Chandler May <cjmay4754@gmail.com>
2017-07-11 16:23:54 -0400

    save memory usage

--- time.bash
+++ time.bash
@@ -11,12 +11,12 @@ echo "$0:"
 echo "Running $num_trials trials"
 echo "Writing output to $output_path"
 
-echo 'wallclock_seconds	system_cpu_seconds	user_cpu_seconds	cpu_percentage	exit_status	major_faults	minor_faults	swaps	context_switches	output_path	command' > "$output_path"
+echo 'wallclock_seconds	system_cpu_seconds	user_cpu_seconds	cpu_percentage	exit_status	major_faults	minor_faults	swaps	context_switches	avg_rss_kilobytes	output_path	command' > "$output_path"
 for i in $(seq 1 $num_trials)
 do
     echo
     echo '$' "$@"
-    /usr/bin/time -a -o "$output_path" -f "%e	%S	%U	%P	%x	%F	%R	%W	%c	$output_path	%C" "$@"
+    /usr/bin/time -a -o "$output_path" -f "%e	%S	%U	%P	%x	%F	%R	%W	%c	%t	$output_path	%C" "$@"
 done
 
 echo
