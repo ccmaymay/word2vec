ba065d6b404d88d3ab14590242eca7293eec9e34
Chandler May <cjmay4754@gmail.com>
2017-07-11 17:18:40 -0400

    explicit negative sampling in gensim

--- gensim-word2vec.py
+++ gensim-word2vec.py
@@ -26,7 +26,7 @@ def main():
         print('initializing vocab')
         model = Word2Vec(size=100, window=5, min_count=5, workers=1,
                          alpha=0.025, sample=0.001, min_alpha=0.025 * 0.0001,
-                         negative=5, iter=1, sg=1)
+                         negative=5, iter=1, sg=1, hs=0)
         print('building vocab')
         model.build_vocab(sentences)
         print('saving vocab')
