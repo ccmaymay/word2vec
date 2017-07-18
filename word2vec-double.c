4b2d5833a20faa71fe111bbc3174c1bca15a2e2b
Chandler May <cjmay4754@gmail.com>
2017-07-17 21:54:05 -0400

    fix word2vec-double model save

--- word2vec-double.c
+++ word2vec-double.c
@@ -568,13 +568,17 @@ void TrainModel() {
   start = clock();
   for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
   for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
+  float x;
   fo = fopen(output_file, "wb");
   if (classes == 0) {
     // Save the word vectors
     fprintf(fo, "%lld %lld\n", vocab_size, layer1_size);
     for (a = 0; a < vocab_size; a++) {
       fprintf(fo, "%s ", vocab[a].word);
-      if (binary) for (b = 0; b < layer1_size; b++) fwrite(&syn0[a * layer1_size + b], sizeof(real), 1, fo);
+      if (binary) for (b = 0; b < layer1_size; b++) {
+        x = (float) syn0[a * layer1_size + b];
+        fwrite(&x, sizeof(float), 1, fo);
+      }
       else for (b = 0; b < layer1_size; b++) fprintf(fo, "%lf ", syn0[a * layer1_size + b]);
       fprintf(fo, "\n");
     }
