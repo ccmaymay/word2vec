69b1e219225a8ef0bc005873c925e3918cd405f4
Chandler May <cjmay@users.noreply.github.com>
2017-07-20 17:52:53 -0400

    Improve comments in distance.c

--- distance.c
+++ distance.c
@@ -49,7 +49,9 @@ int main(int argc, char **argv) {
     printf("Cannot allocate memory: %lld MB    %lld  %lld\n", (long long)words * size * sizeof(float) / 1048576, words, size);
     return -1;
   }
+  // read word vectors into array M
   for (b = 0; b < words; b++) {
+    // read word
     a = 0;
     while (1) {
       vocab[b * max_w + a] = fgetc(f);
@@ -57,18 +59,22 @@ int main(int argc, char **argv) {
       if ((a < max_w) && (vocab[b * max_w + a] != '\n')) a++;
     }
     vocab[b * max_w + a] = 0;
-    /* read l2-normalized word vectors */
+    // read word vector
     for (a = 0; a < size; a++) fread(&M[a + b * size], sizeof(float), 1, f);
+    // l2-normalize word vector
     len = 0;
     for (a = 0; a < size; a++) len += M[a + b * size] * M[a + b * size];
     len = sqrt(len);
     for (a = 0; a < size; a++) M[a + b * size] /= len;
   }
   fclose(f);
+  // start read-eval-print loop
   while (1) {
     for (a = 0; a < N; a++) bestd[a] = 0;
     for (a = 0; a < N; a++) bestw[a][0] = 0;
     printf("Enter word or sentence (EXIT to break): ");
+    // read input phrase into st1
+    // TODO: check for EOF
     a = 0;
     while (1) {
       st1[a] = fgetc(stdin);
@@ -79,6 +85,8 @@ int main(int argc, char **argv) {
       a++;
     }
     if (!strcmp(st1, "EXIT")) break;
+    // tokenize (by spaces) input phrase in st1 into individual words
+    // (store in st, an array of strings)
     cn = 0;
     b = 0;
     c = 0;
@@ -94,7 +102,10 @@ int main(int argc, char **argv) {
         c++;
       }
     }
+    // increment cn so that it represents the number of words read
     cn++;
+    // look up each word in phrase, storing the vocabulary
+    // indices in array bi (respectively)
     for (a = 0; a < cn; a++) {
       for (b = 0; b < words; b++) if (!strcmp(&vocab[b * max_w], st[a])) break;
       if (b == words) b = -1;
@@ -107,51 +118,50 @@ int main(int argc, char **argv) {
     }
     if (b == -1) continue;
     printf("\n                                              Word       Cosine distance\n------------------------------------------------------------------------\n");
-    /* set vec to sum of vectors in input sentence */
+    // set vec to sum of (normalized) vectors of words in input sentence
     for (a = 0; a < size; a++) vec[a] = 0;
     for (b = 0; b < cn; b++) {
       if (bi[b] == -1) continue;
       for (a = 0; a < size; a++) vec[a] += M[a + bi[b] * size];
     }
-    /* compute len = l2 norm of vec */
+    // l2-normalize vec
     len = 0;
     for (a = 0; a < size; a++) len += vec[a] * vec[a];
     len = sqrt(len);
-    /* normalize vec */
     for (a = 0; a < size; a++) vec[a] /= len;
-    /* find closest words to input sentence that are not in sentence */
-    /* bestd will be array of N highest (squared) cosines against
-     * sentence vector */
+    // find closest words to input sentence that are not in sentence;
+    // bestd will be array of N highest (squared) cosines against
+    // sentence vector
     for (a = 0; a < N; a++) bestd[a] = -1;
-    /* bestw will be array of corresponding words words */
+    // bestw will be array of corresponding words words
     for (a = 0; a < N; a++) bestw[a][0] = 0;
     for (c = 0; c < words; c++) {
       a = 0;
-      /* if candidate word is in sentence, skip */
+      // if candidate word is in sentence, skip
       for (b = 0; b < cn; b++) if (bi[b] == c) a = 1;
       if (a == 1) continue;
       dist = 0;
-      /* compute (squared) cosine of candidate word's vector against
-       * input sentence vector */
+      // compute (squared) cosine of candidate word's vector against
+      // input sentence vector
       for (a = 0; a < size; a++) dist += vec[a] * M[a + c * size];
-      /* set candidate word in appropriate place in top-N lists */
+      // set candidate word in appropriate place in top-N lists */
       for (a = 0; a < N; a++) {
-        /* if candidate word is closer than a-th word in top-N list,
-         * replace and break */
+        // if candidate word is closer than a-th word in top-N list,
+        // replace and break
         if (dist > bestd[a]) {
-          /* shift current words down in rank */
+          // shift current words down in rank
           for (d = N - 1; d > a; d--) {
             bestd[d] = bestd[d - 1];
             strcpy(bestw[d], bestw[d - 1]);
           }
-          /* set a-th top word to candidate word */
+          // set a-th top word to candidate word
           bestd[a] = dist;
           strcpy(bestw[a], &vocab[c * max_w]);
           break;
         }
       }
     }
-    /* print results */
+    // print results
     for (a = 0; a < N; a++) printf("%50s\t\t%f\n", bestw[a], bestd[a]);
   }
   return 0;
