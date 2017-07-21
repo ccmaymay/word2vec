93983d455025f874177ffc830e36c477ac3fa1c5
Chandler May <cjmay4754@gmail.com>
2017-07-21 15:03:57 -0400

    update to new api

--- word2vec-athena-13.cpp
+++ word2vec-athena-13.cpp
@@ -31,6 +31,7 @@ using namespace std;
 const int vocab_hash_size = 30000000;  // Maximum 30 * 0.7 = 21M words in the vocabulary
 
 typedef float real;                    // Precision of float numbers
+typedef SGNSTokenLearner<NaiveLanguageModel, DiscreteSamplingStrategy<NaiveLanguageModel> > SGNSTokenLearnerType;
 
 char train_file[MAX_STRING], output_file[MAX_STRING];
 char save_vocab_file[MAX_STRING], read_vocab_file[MAX_STRING];
@@ -205,7 +206,7 @@ long long read_new_sentence(FILE* fi, const NaiveLanguageModel& language_model,
   return sentence_length;
 }
 
-void TrainModelThread(SGNSTokenLearner<ReservoirSamplingStrategy<NaiveLanguageModel>,NaiveLanguageModel>& token_learner, ContextStrategy& ctx_strategy, int neg_samples) {
+void TrainModelThread(SGNSTokenLearnerType& token_learner, NaiveLanguageModel& language_model, DynamicContextStrategy& ctx_strategy, SGD& sgd, int neg_samples) {
   long long
     sentence_length = 0,
     input_word_position = 0,
@@ -217,16 +218,16 @@ void TrainModelThread(SGNSTokenLearner<ReservoirSamplingStrategy<NaiveLanguageMo
   while (1) {
     if (word_count - last_word_count > 10000) {
       word_count_actual += word_count - last_word_count;
-      update_progress(token_learner.language_model, token_learner.sgd);
+      update_progress(language_model, sgd);
       last_word_count = word_count;
     }
     char eof = 0;
     if (input_word_position >= sentence_length) {
-      sentence_length = read_new_sentence(fi, token_learner.language_model, &word_count,
+      sentence_length = read_new_sentence(fi, language_model, &word_count,
                                           sen, &eof);
       input_word_position = 0;
     }
-    if (eof || (word_count > token_learner.language_model.total() / num_threads)) {
+    if (eof || (word_count > language_model.total() / num_threads)) {
       word_count_actual += word_count - last_word_count;
       local_iter--;
       if (local_iter == 0) break;
@@ -251,53 +252,58 @@ void TrainModelThread(SGNSTokenLearner<ReservoirSamplingStrategy<NaiveLanguageMo
       token_learner.token_train(input_word, output_word, neg_samples);
     }
     input_word_position++;
-    token_learner.sgd.step(input_word);
+    sgd.step(input_word);
   }
   fclose(fi);
 }
 
-void TrainModel(NaiveLanguageModel& language_model, real alpha, long long embedding_size, int window, int neg_samples) {
+void TrainModel(shared_ptr<NaiveLanguageModel> language_model, real alpha, long long embedding_size, int window, int neg_samples) {
   printf("Starting training using file %s\n", train_file);
-  if (read_vocab_file[0] != 0) ReadVocab(language_model); else LearnVocabFromTrainFile(language_model);
-  if (save_vocab_file[0] != 0) SaveVocab(language_model);
+  if (read_vocab_file[0] != 0) ReadVocab(*language_model); else LearnVocabFromTrainFile(*language_model);
+  if (save_vocab_file[0] != 0) SaveVocab(*language_model);
   if (output_file[0] == 0) return;
 
-  size_t lm_size(language_model.size());
-  size_t lm_total(language_model.total());
-  SGNSTokenLearner<ReservoirSamplingStrategy<NaiveLanguageModel>,NaiveLanguageModel> token_learner(
-    WordContextFactorization(lm_size, embedding_size),
-    ReservoirSamplingStrategy<NaiveLanguageModel>(ReservoirSampler<long>(table_size)),
-    move(language_model),
-    SGD(lm_size, lm_total * iter, alpha, 0.0001 * alpha));
+  size_t lm_size(language_model->size());
+  size_t lm_total(language_model->total());
+  ExponentCountNormalizer normalizer(0.75);
+  auto sgd(make_shared<SGD>(lm_size, lm_total * iter, alpha, 0.0001 * alpha));
+  auto factorization(make_shared<WordContextFactorization>(lm_size, embedding_size));
+  SGNSTokenLearnerType token_learner(
+    factorization,
+    make_shared<DiscreteSamplingStrategy<NaiveLanguageModel> >(
+      make_shared<Discretization>(
+        normalizer.normalize(language_model->counts()),
+        table_size)),
+    language_model,
+    sgd
+  );
   DynamicContextStrategy ctx_strategy(window);
-  CountNormalizer normalizer(0.75, 0);
-  token_learner.neg_sampling_strategy.reset(token_learner.language_model, normalizer);
 
   start = clock();
-  TrainModelThread(token_learner, ctx_strategy, neg_samples);
+  TrainModelThread(token_learner, *language_model, ctx_strategy, *sgd, neg_samples);
   FILE *fo = fopen(output_file, "wb");
   if (classes == 0) {
     // Save the word vectors
-    fprintf(fo, "%zu %lld\n", token_learner.language_model.size(), embedding_size);
-    for (long a = 0; a < token_learner.language_model.size(); a++) {
-      fprintf(fo, "%s ", token_learner.language_model.reverse_lookup(a).c_str());
-      if (binary) for (long b = 0; b < embedding_size; b++) fwrite(token_learner.factorization.get_word_embedding(a) + b, sizeof(real), 1, fo);
-      else for (long b = 0; b < embedding_size; b++) fprintf(fo, "%lf ", token_learner.factorization.get_word_embedding(a)[b]);
+    fprintf(fo, "%zu %lld\n", lm_size, embedding_size);
+    for (long a = 0; a < lm_size; a++) {
+      fprintf(fo, "%s ", language_model->reverse_lookup(a).c_str());
+      if (binary) for (long b = 0; b < embedding_size; b++) fwrite(factorization->get_word_embedding(a) + b, sizeof(real), 1, fo);
+      else for (long b = 0; b < embedding_size; b++) fprintf(fo, "%lf ", factorization->get_word_embedding(a)[b]);
       fprintf(fo, "\n");
     }
   } else {
     // Run K-means on the word vectors
     int clcn = classes, iter = 10, closeid;
     int *centcn = (int *)malloc(classes * sizeof(int));
-    int *cl = (int *)calloc(token_learner.language_model.size(), sizeof(int));
+    int *cl = (int *)calloc(lm_size, sizeof(int));
     real closev, x;
     real *cent = (real *)calloc(classes * embedding_size, sizeof(real));
-    for (long a = 0; a < token_learner.language_model.size(); a++) cl[a] = a % clcn;
+    for (long a = 0; a < lm_size; a++) cl[a] = a % clcn;
     for (long a = 0; a < iter; a++) {
       for (long b = 0; b < clcn * embedding_size; b++) cent[b] = 0;
       for (long b = 0; b < clcn; b++) centcn[b] = 1;
-      for (long c = 0; c < token_learner.language_model.size(); c++) {
-        for (long d = 0; d < embedding_size; d++) cent[embedding_size * cl[c] + d] += token_learner.factorization.get_word_embedding(c)[d];
+      for (long c = 0; c < lm_size; c++) {
+        for (long d = 0; d < embedding_size; d++) cent[embedding_size * cl[c] + d] += factorization->get_word_embedding(c)[d];
         centcn[cl[c]]++;
       }
       for (long b = 0; b < clcn; b++) {
@@ -309,12 +315,12 @@ void TrainModel(NaiveLanguageModel& language_model, real alpha, long long embedd
         closev = sqrt(closev);
         for (long c = 0; c < embedding_size; c++) cent[embedding_size * b + c] /= closev;
       }
-      for (long c = 0; c < token_learner.language_model.size(); c++) {
+      for (long c = 0; c < lm_size; c++) {
         closev = -10;
         closeid = 0;
         for (long d = 0; d < clcn; d++) {
           x = 0;
-          for (long b = 0; b < embedding_size; b++) x += cent[embedding_size * d + b] * token_learner.factorization.get_word_embedding(c)[b];
+          for (long b = 0; b < embedding_size; b++) x += cent[embedding_size * d + b] * factorization->get_word_embedding(c)[b];
           if (x > closev) {
             closev = x;
             closeid = d;
@@ -324,7 +330,7 @@ void TrainModel(NaiveLanguageModel& language_model, real alpha, long long embedd
       }
     }
     // Save the K-means classes
-    for (long a = 0; a < token_learner.language_model.size(); a++) fprintf(fo, "%s %d\n", token_learner.language_model.reverse_lookup(a).c_str(), cl[a]);
+    for (long a = 0; a < lm_size; a++) fprintf(fo, "%s %d\n", language_model->reverse_lookup(a).c_str(), cl[a]);
     free(centcn);
     free(cent);
     free(cl);
@@ -411,7 +417,7 @@ int main(int argc, char **argv) {
     printf("Must have num_threads = 1\n");
     exit(1);
   }
-  NaiveLanguageModel language_model(sample);
+  auto language_model(make_shared<NaiveLanguageModel>(sample));
   TrainModel(language_model, alpha, embedding_size, window, neg_samples);
   return 0;
 }
